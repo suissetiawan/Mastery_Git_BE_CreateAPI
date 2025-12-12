a3f99a166b1baeebf53b2fffc3dd8712ddc013ff
suis <suissetiawan11@gmail.com>
2025-12-12 20:57:25 +0700

    feat/initialize project

---
+++ src/main/java/com/dibimbing/assignmentDay20/AssignmentDay20Application.java
@@ -0,0 +1,13 @@
+package com.dibimbing.assignmentDay20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentDay20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentDay20Application.class, args);
+	}
+
+}
